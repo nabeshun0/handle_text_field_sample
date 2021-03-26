eccfa0c6ff2e3fa34b50b7af09489bdf5342b29e
snt-nbzw <sntnbzw@gmail.com>
2021-03-26 11:37:06 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/nabeshun/handle_text_field_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nabeshun.handle_text_field_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Retrieve Text Input',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Retrieve Text Input')),
+    );
+  }
+}
