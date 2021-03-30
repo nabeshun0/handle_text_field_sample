6899b0e63946a2dde8fece7f83a08984438625f9
snt-nbzw <sntnbzw@gmail.com>
2021-03-30 10:35:54 +0900

    Add: listen to the controller for changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,44 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    print('Second text field: ${myController.text}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Retrieve Text Input')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text) {
+                print('First text field: $text');
+              },
+            ),
+            TextField(
+              controller: myController,
+            ),
+            Text('data'),
+          ],
+        ),
+      ),
     );
   }
 }
